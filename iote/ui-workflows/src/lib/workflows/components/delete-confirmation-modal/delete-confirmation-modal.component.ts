3b4369be3138166793c455c673adb5ec9ff808bd
Jente Rosseel <rosseeljente@gmail.com>
2020-03-24 17:37:05 +0100

    Delete Confirmation Modal -Remove encapsulation leak

--- iote/ui-workflows/src/lib/workflows/components/delete-confirmation-modal/delete-confirmation-modal.component.ts
+++ iote/ui-workflows/src/lib/workflows/components/delete-confirmation-modal/delete-confirmation-modal.component.ts
@@ -1,16 +1,25 @@
-import { Component, ViewEncapsulation, Inject } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
+/**
+ * Delete Dialog.
+ *
+ * For no-pad mode,
+ *    - inject { panelClass: 'modal-no-pad' } from calling parent.
+ *    - inject data property noPad as true
+ *    - Ensure .modal-no-pad .mat-dialog-container { padding: 0 } is part of theme
+ *
+ * @class DeleteConfirmationDialogComponent
+ */
 @Component({
   selector: 'iote-delete-confirmation-modal',
   templateUrl: 'delete-confirmation-modal.component.html',
-  styleUrls: ['delete-confirmation-modal.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['delete-confirmation-modal.component.scss']
 })
-export class DeleteConfirmationDialogComponent {
-
+export class DeleteConfirmationDialogComponent
+{
   constructor(public dialogRef: MatDialogRef<DeleteConfirmationDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any)
+              @Inject(MAT_DIALOG_DATA) public data: { noPad?: boolean, content: string })
   { }
 
   closeModal(result: boolean): void {
