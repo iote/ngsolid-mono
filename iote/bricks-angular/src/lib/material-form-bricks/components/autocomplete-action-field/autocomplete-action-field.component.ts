76afb10164e0a98bf44dca1a45125abd4b9de322
Jente Rosseel <rosseeljente@gmail.com>
2020-07-21 14:47:41 +0200

    Add sorting and filtering to single autocomplete

--- iote/bricks-angular/src/lib/material-form-bricks/components/autocomplete-action-field/autocomplete-action-field.component.ts
+++ iote/bricks-angular/src/lib/material-form-bricks/components/autocomplete-action-field/autocomplete-action-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, EventEmitter, Output, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'iote-autocomplete-action-field',
@@ -8,6 +9,7 @@ import { Component, Input, EventEmitter, Output, OnInit, OnChanges, SimpleChange
 export class AutocompleteActionFieldComponent<T> implements OnInit, OnChanges
 {
   @Input() items: T[];
+  displayItems: T[];
   selectedItemNow: string;
   @Input() selectedItem: T;
   @Input() itemFieldDisplayFn: (T) => string;
@@ -17,11 +19,16 @@ export class AutocompleteActionFieldComponent<T> implements OnInit, OnChanges
   @Output() itemSelected = new EventEmitter<T>();
   @Output() newItemTyped  = new EventEmitter<string>();
 
+  private _filter: string;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
     if(this.selectedItem)
       this.selectedItemNow = this.itemFieldDisplayFn(this.selectedItem);
+
+    this.displayItems = this._getItems(this.items);
   };
 
   ngOnChanges(changes: SimpleChanges)
@@ -30,7 +37,7 @@ export class AutocompleteActionFieldComponent<T> implements OnInit, OnChanges
     const selectedItem = changes['selectedItem'];
 
     if(items)
-      this.items = items.currentValue;
+      this.displayItems = this._getItems(items.currentValue);
 
     if(selectedItem) {
       this.selectedItem = selectedItem.currentValue;
@@ -41,6 +48,9 @@ export class AutocompleteActionFieldComponent<T> implements OnInit, OnChanges
   /** Unknown item typed -> Emit event that requests parent to create item. */
   onTypeSelectedItem(newItemName: any)
   {
+    this._filter = newItemName;
+    this.displayItems = this._getItems(this.items);
+
     this.newItemTyped.emit(newItemName.target.value);
   }
 
@@ -52,4 +62,10 @@ export class AutocompleteActionFieldComponent<T> implements OnInit, OnChanges
     this.itemSelected.emit(item);
   }
 
+  private _getItems = (items: T[]) => this.displayItems = this._sortFn(this._filterFn(items));
+
+  private _filterFn = (items: T[]) => _.filter(items, i => this._getName(i).replace(' ', '').toLowerCase().indexOf(this._filter.replace(' ', '').toLowerCase())) as T[];
+  private _sortFn   = (items: T[]) => _.orderBy(items, i => this.itemFieldDisplayFn ? this.itemFieldDisplayFn(i) : i, 'asc');
+
+  private _getName  = (item: T)    => this.itemFieldDisplayFn ? this.itemFieldDisplayFn(item) : (item as any) as string;
 }
