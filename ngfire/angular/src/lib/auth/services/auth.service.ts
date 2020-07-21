b0b54619d1f2cfe072fdf8aa5d71f5ee6c22647f
Jente Rosseel <rosseeljente@gmail.com>
2020-07-21 17:41:14 +0200

    Revert "Migrate to angular/fire v6"
    This reverts commit b28792efb0001b1cea21e07c3d2d5fcad605cf42.

--- ngfire/angular/src/lib/auth/services/auth.service.ts
+++ ngfire/angular/src/lib/auth/services/auth.service.ts
@@ -25,7 +25,7 @@ export class AuthService {
   { }
 
   public createUserWithEmailAndPassword(displayName: string, email: string, password: string, userProfile: UserProfile, roles: Roles) {
-    return this.afAuth.createUserWithEmailAndPassword(email, password)
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then((res) => {
         this._updateUserData(res.user, displayName, userProfile, roles);
         return <User> <unknown> res.user;
@@ -39,7 +39,7 @@ export class AuthService {
 
     return new Promise((resolve, reject) => {
 
-      this.afAuth.signInWithEmailAndPassword(email, password)
+      this.afAuth.auth.signInWithEmailAndPassword(email, password)
         .then(() => {
           this._logger.log(() => `AuthService.signInWithEmailAndPassword: Successfully logged user in with Email and Password.`);
 
@@ -75,7 +75,7 @@ export class AuthService {
   }
 
   private _oAuthLogin(provider: auth.AuthProvider) {
-    return this.afAuth
+    return this.afAuth.auth
       .signInWithPopup(provider)
       .then((credential) => {
         this._logger.log(() => "Successful firebase user sign in");
@@ -133,7 +133,7 @@ export class AuthService {
 
   signOut(route?: string)
   {
-    return this.afAuth.signOut().then(() => {
+    return this.afAuth.auth.signOut().then(() => {
       this.router.navigate([route ?? '/']);
     });
   }
