42ca6007a1fc5753d7151b2218e9eeb6b180b47c
Paul Chesa <paulchesa1@gmail.com>
2020-09-07 15:29:09 +0300

    update file storage service

--- ngfire/files/src/lib/files/services/file-storage.service.ts
+++ ngfire/files/src/lib/files/services/file-storage.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from "@angular/core";
+import { Injectable, Input } from "@angular/core";
 
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from '@iote/bricks';
 import {  Repository, UserService } from '@ngfire/angular';
 
 import { IFile } from '../model/file.interface';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+
+
 
 @Injectable()
 export class FileStorageService
 {
   private _fileRepository: Repository<IFile>;
 
-  constructor(db: AngularFirestore, private _userService: UserService<User>)
+
+
+  constructor(db: AngularFirestore, private _userService: UserService<User>, private fireStorage: AngularFireStorage)
   {
     this._fileRepository = new Repository<IFile>('files', db, this._userService);
+  }
+
+
+
+
+  uploadSingleFile(event, n) {
+    const file = event.target.files[0];
+    const task = this.fireStorage.upload(`profileImages/${n}`, file);
+    return task.snapshotChanges()
   }
 
   createFileRef(path, name, type, description)
