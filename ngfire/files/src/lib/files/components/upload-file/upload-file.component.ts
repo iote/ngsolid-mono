40d03a36c68e2d8b834de1229f49b55433242025
DMG <dmwaigithinji@gmail.com>
2020-10-08 16:42:39 +0300

    Revert "Enable brick upload file to accept more than one file type"
    This reverts commit c2368575c30db424787ccc1c23eebab57f592c6b.

--- ngfire/files/src/lib/files/components/upload-file/upload-file.component.ts
+++ ngfire/files/src/lib/files/components/upload-file/upload-file.component.ts
@@ -29,9 +29,7 @@ import { FileStorageService } from '../../services/file-storage.service';
 export class UploadFileComponent implements OnInit
 {
   @Input()  title = 'Upload a file';
-  @Input()  types: string[] = ['any'];
-
-  uploadedType: string;
+  @Input()  type  = 'any';
 
   @Input() filePath: string;
   @Input() prepareFilePathFn: (string) => string;
@@ -61,33 +59,22 @@ export class UploadFileComponent implements OnInit
       this.error = null;
 
     const file = event.target.files[0];
+
     if (this._validateUpload(file))
       this._doUpload(file);
   }
 
   _validateUpload(file)
   {
+    switch (this.type) {
+                // Or will be executed lazy -> If condition is false or will be executed.
+      case 'image':
+        return file.type.split('/')[0] === 'image' || this._validationError('Error uploading file. Only images allowed.');
 
-    this.uploadedType = this._getFileType(file);
-
-    const isValid = this.types === ['any'] || this.types.includes(this.uploadedType)
-                      ? true
-                      : this._validationError(`Error uploading file. Only files of ${this.types.join('/ ')} format allowed.`);
-
-    return isValid;
-
-    // switch (this.type) {
-    //             // Or will be executed lazy -> If condition is false or will be executed.
-    //   case 'image':
-    //     return file.type.split('/')[0] === 'image' || this._validationError('Error uploading file. Only images allowed.');
-
-    //   case 'pdf':
-    //     return file.type.split('/')[0] === 'image' || this._validationError('Error uploading file. Only images allowed.');
-
-    //   case 'any':
-    //   default:
-    //     return true;
-    // }
+      case 'any':
+      default:
+        return true;
+    }
   }
 
   private _doUpload(file)
@@ -113,7 +100,7 @@ export class UploadFileComponent implements OnInit
 
         // Save the path and name of the image to the firestore database
         this._fileStorageService
-              .createFileRef(path, file.name, this.uploadedType, this.description)
+              .createFileRef(path, file.name, this.type, this.description)
               .subscribe(storedFile => this._fileCreated({ file: storedFile, downloadLink: this.downloadURL$ }));
 
         this._logger.log(() => `The upload was successful ${ this.downloadURL$ }.`);
@@ -133,15 +120,5 @@ export class UploadFileComponent implements OnInit
     this._logger.log(() => eMsg);
     this.error = eMsg;
     return false;
-  }
-
-  private _getFileType(file)
-  {
-    if (file.type.split('/')[0] === 'image')
-      return 'image';
-    else if (file.type.split('/')[1] === 'pdf')
-      return 'pdf'
-    else
-      return 'any';
   }
 }
