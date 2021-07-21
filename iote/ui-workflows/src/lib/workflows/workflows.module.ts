82c2fa852aa47a837bbcfd93dfee72253b20cec1
Jente Rosseel <rosseeljente@gmail.com>
2021-07-21 13:05:52 +0300

    📦 Link workflow-module to ngfire

--- iote/ui-workflows/src/lib/workflows/workflows.module.ts
+++ iote/ui-workflows/src/lib/workflows/workflows.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MultiLangModule } from '@elewa/elements/multi-lang';
+import { MultiLangModule } from '@ngfire/multi-lang';
 
 
 import { MaterialDesignModule } from '@iote/bricks-angular';
