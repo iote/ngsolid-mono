41f139287bc72588c723cef7e3912d0088885437
Ess Bidali <51133968+Ess-Bidali@users.noreply.github.com>
2021-08-11 13:05:52 +0300

    📦 Added abstract Base Optimistic event store

---
+++ iote/optimistic-ui/src/lib/store/base-optimistic-event-store.class.ts
@@ -0,0 +1,20 @@
+import { EntityStore } from "@iote/state";
+
+import { Observable } from "rxjs";
+
+import { OptimisticEvent } from "../model/optimistic-event.model";
+
+/**
+ * @class BaseOptimisticEventsStore
+ *
+ * @description  Base class providing guidelines for setting up an Optimistic Ui Event Store
+ *
+ */
+export abstract class BaseOptimisticEventsStore extends EntityStore<OptimisticEvent>
+{
+  /**
+   * @param storeName Name of the store. (Helps filter out irrelevant events that do not belong to the specific store)
+   * @param filter Filter applied to the original store data/query
+   */
+  abstract getSimulated<T>(storeName: string, filter? : (t: T) => boolean): Observable<T[]>;
+}
