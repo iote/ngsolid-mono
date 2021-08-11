2aa4e42cf016bea4bcd3fa9568534fbd8d8e8831
Ess Bidali <51133968+Ess-Bidali@users.noreply.github.com>
2021-08-11 15:15:21 +0300

    Added base optimistic event store to exports

--- iote/optimistic-ui/src/index.ts
+++ iote/optimistic-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/optimistic-ui.module';
 export * from './lib/store/optimistic-data-store.class';
+export * from './lib/store/base-optimistic-event-store.class';
 export * from './lib/store/optimistic-ui-events.store';
