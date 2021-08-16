fcf209dab6845a2e41da2e0d33af67acb9edfdbf
Ess Bidali <51133968+Ess-Bidali@users.noreply.github.com>
2021-08-16 16:41:57 +0300

    📦 Prevent leaking of optimistic effect attributes

--- iote/optimistic-ui/src/lib/bridge/i-bridge.interface.ts
+++ iote/optimistic-ui/src/lib/bridge/i-bridge.interface.ts
@@ -1,8 +1,10 @@
 import { DbMethods } from '@iote/cqrs';
 import { IObject } from '@iote/bricks';
 import { ICommand } from '@iote/cqrs';
-import { IOptimisticEffectsStore } from '../store/i-optimistic-effects-store.class';
 
+import * as _ from 'lodash';
+
+import { IOptimisticEffectsStore } from '../store/i-optimistic-effects-store.class';
 import { IOptimisticEffect } from './../model/i-optimistic-effect.model';
 
 export interface IBridge<C extends ICommand<P>, P extends IObject>
@@ -21,11 +23,15 @@ export abstract class Bridge<C extends ICommand<P>, P extends IObject> implement
   {
   }
 
+  abstract execute(command: C) : P[]
+
   run(command: C): IOptimisticEffect<P>[]
   {
     const events = this.execute(command).map(obj => this._createEvent(obj, command));
 
     this._eventsStore$$.add(events);
+
+    setTimeout(() => this.revert(command), this._duration);
 
     return events;
   }
@@ -39,10 +45,10 @@ export abstract class Bridge<C extends ICommand<P>, P extends IObject> implement
     return events;
   }
 
-  abstract execute(command: C) : P[]
-
-  private _createEvent(obj: P, command: C): IOptimisticEffect<P>
+  private _createEvent(payload: P, command: C): IOptimisticEffect<P>
   {
+    const obj = _.cloneDeep(payload);
+
     (obj as any).isOptimistic = true;
 
     (obj as any).del = command.method === DbMethods.DELETE;
