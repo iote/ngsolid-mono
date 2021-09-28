76557f401f14a79a0ebc909ea3a2e032c5e8b49f
Jente Rosseel <rosseeljente@gmail.com>
2021-09-28 19:22:19 +0300

    :package: :wrench: Re-package @iote/bricks-angular as Ivy-compatible

--- iote/bricks-angular/jest.config.js
+++ iote/bricks-angular/jest.config.js
@@ -1,15 +1,20 @@
 module.exports = {
   displayName: 'external-iote-bricks-angular',
   preset: '../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/external/iote/bricks-angular',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
