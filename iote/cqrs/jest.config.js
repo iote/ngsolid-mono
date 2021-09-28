daed2f4425fe259cfe1aea92296d6af60686702a
Jente Rosseel <rosseeljente@gmail.com>
2021-09-28 18:01:26 +0300

    :package: :wrench: Upgrade @iote/cqrs to publishable

--- iote/cqrs/jest.config.js
+++ iote/cqrs/jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  name: 'external-iote-cqrs',
-  preset: '../../../../jest.config.js',
+  displayName: 'external-iote-cqrs',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/external/iote/cqrs',
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
