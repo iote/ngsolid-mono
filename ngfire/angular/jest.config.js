5b8503d367bbb56eb598f61283c2db08bb3da6c8
Jente Rosseel <rosseeljente@gmail.com>
2021-10-04 10:52:46 +0300

    :package: Publishable @ngfi/angular

--- ngfire/angular/jest.config.js
+++ ngfire/angular/jest.config.js
@@ -1,23 +1,20 @@
 module.exports = {
+  displayName: 'external-ngfire-angular',
   preset: '../../../../jest.preset.js',
-  coverageDirectory: '../../../../coverage/libs/external/ngfire/angular',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
+      tsconfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: {
-        before: [
-          'jest-preset-angular/build/InlineFilesTransformer',
-          'jest-preset-angular/build/StripStylesTransformer',
-        ],
-      },
     },
   },
-  displayName: '@ngfire/angular',
+  coverageDirectory: '../../../../coverage/libs/external/ngfire/angular',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
