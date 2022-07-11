74c9f06f7f0512f96e955f84b6424b94dce5c3e6
Philip Mutua <pmutua@live.com>
2022-07-11 07:22:10 +0300

    :package: feat: :sparkles: Add route keywords  Ref#16

---
+++ iote/analytics/src/lib/data/route-keywords.ts
@@ -0,0 +1,14 @@
+/**
+ * Add a list of your applicaton's routes to the @ROUTE_KEYWORDS array.
+ * There defined in every routes module.
+ * 
+ * e.g  ['home','detail','overview','accounting','reports','settings','user'];
+ */
+export const ROUTE_KEYWORDS = [
+    'financial-years', 'overview', 'detail',
+    'owners', 'overview', 'reserve', 'wcac',
+    'preparation', 'info', 'accounts', 'supplier',
+    'suppliers', 'owner', 'transfers', 'savings',
+    'transactions', 'provisions', 'building-details',
+    'keys', 'transfers', 'financial-years', 'currents',
+    'assembly'];
