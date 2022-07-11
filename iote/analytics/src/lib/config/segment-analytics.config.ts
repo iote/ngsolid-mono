86ec8578c19bff701f054ed3b2bda599b6c252f0
Philip Mutua <pmutua@live.com>
2022-07-11 07:21:13 +0300

    :package: feat: :sparkles: Add segment integration & segment service Ref#16
    - Also Added a window wrapper to be accessed globally

---
+++ iote/analytics/src/lib/config/segment-analytics.config.ts
@@ -0,0 +1,27 @@
+import {InjectionToken} from '@angular/core';
+
+/**
+ * Segment Configuration Interface
+ */
+export interface SegmentConfig {
+    /** API Key for Segment.io */
+    apiKey?: string;
+    /** Debug mode status */
+    debug?: boolean;
+    /** Load Segment configuration on initialization */
+    loadOnInitialization?: boolean;
+    /** Segment Host if requests are proxied **/
+    segmentHost?: string;
+    /** Segment Custom URI **/
+    segmentUri?: string;
+}
+
+/** Segment Configuration Injection Token */
+export const SEGMENT_CONFIG: InjectionToken<SegmentConfig> = new InjectionToken<SegmentConfig>('segment-analytics.config');
+
+export const DEFAULT_CONFIG: SegmentConfig = {
+    debug: false,
+    loadOnInitialization: true, // Compatibility < 1.2.5
+    segmentHost: 'cdn.segment.com',
+    segmentUri: '/analytics.js/v1/$API_KEY$/analytics.min.js',
+};
